695ea163c374dae4945dc57bf0642a418c20636e
Anurag9788 <anuraganand2020@outlook.com>
2021-03-22 00:14:49 +0530

    Add files via upload

---
+++ commonHelpers.h
@@ -0,0 +1,10 @@
+int currentYear(void);
+void clearStanadardInputBuffer(void);
+int getInteger(void);
+int getPositiveInteger(void);
+double getDouble();
+double getPositiveDouble();
+int getIntFromRange(int lower_bound,int upper_bound);
+char getCharOption(char input[]);
+void getCString(char *ptr,int lower_bound,int upper_bound);
+
